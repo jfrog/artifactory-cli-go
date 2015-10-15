828d321dd0fbab34a968c64665981fb04d9a8af9
shikloshi <aviads@jfrog.com>
2015-10-15 09:35:27 +0300

    adding build and deployment pipeline

---
+++ prepare_bintray_deployment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/travis/gopath/bin
+
+if [ "${GIMME_OS}" != "linux" ] || [ "${GIMME_ARCH}" != "amd64" ] ; then
+	cd "${GIMME_OS}_${GIMME_ARCH}"
+fi
+
+if [ "${GIMME_OS}" = "windows" ] ; then
+	mv artifactory-cli-go.exe art.exe
+	art_executable = "art.exe"
+else 
+	mv artifactory-cli-go art
+	art_executable = "art"
+fi
