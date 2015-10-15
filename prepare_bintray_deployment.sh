b9e528eb9425fdebc02891797be89d4ed3ab0dd1
shikloshi <aviads@jfrog.com>
2015-10-15 15:10:34 +0300

    travis.yml fix

--- prepare_bintray_deployment.sh
+++ prepare_bintray_deployment.sh
@@ -3,7 +3,7 @@
 cd /home/travis/gopath/bin
 
 if [ "${GIMME_OS}" != "linux" ] || [ "${GIMME_ARCH}" != "amd64" ] ; then
-	cd "${GIMME_OS}_${GIMME_ARCH}"
+	cd "${GIMME_OS}_${GIMME_ARCH}" || exit
 fi
 
 if [ "${GIMME_OS}" = "windows" ] ; then
