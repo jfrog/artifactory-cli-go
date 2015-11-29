52e4383525d5e7bf0c2982f5f339322b65d6b2f0
eyalbe4 <eyalbenmoshe@jfrog.com>
2015-11-29 18:05:42 +0200

    Modify build script.

--- prepare_bintray_deployment.sh
+++ prepare_bintray_deployment.sh
@@ -2,12 +2,12 @@
 
 cd /home/travis/gopath/bin
 
+echo a
+ls -R
+
 if [ "${GIMME_OS}" != "linux" ] || [ "${GIMME_ARCH}" != "amd64" ] ; then
 	cd "${GIMME_OS}_${GIMME_ARCH}" || exit
 fi
 
-if [ "${GIMME_OS}" = "windows" ] ; then
-	mv artifactory-cli-go.exe art.exe
-else 
-	mv artifactory-cli-go art
-fi
+echo b
+ls -R
