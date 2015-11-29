1b0ce23e9afc628e77ec9478100cea023e912e65
eyalbe4 <eyalbenmoshe@jfrog.com>
2015-11-29 18:19:03 +0200

    Modify build script.

--- prepare_bintray_deployment.sh
+++ prepare_bintray_deployment.sh
@@ -2,12 +2,3 @@
 
 cd /home/travis/gopath/bin
 
-echo a
-ls -R
-
-if [ "${GIMME_OS}" != "linux" ] || [ "${GIMME_ARCH}" != "amd64" ] ; then
-	cd "${GIMME_OS}_${GIMME_ARCH}" || exit
-fi
-
-echo b
-ls -R
