514232c0464091f8eeb20ab2e42eb21598aa4179
shikloshi <aviads@jfrog.com>
2015-12-01 15:10:54 +0200

    build script

--- prepare_bintray_deployment.sh
+++ prepare_bintray_deployment.sh
@@ -3,12 +3,13 @@ echo a
 ls
 
 cd /home/travis/gopath/bin
-echo "${GOOS}"
-echo "${GOARCH}"
+echo "$GOOS"
+echo "$GOARCH"
 ls
 
-if [ "${GOOS}" = "windows" ] ; then
+if [ "$GOOS" = "windows" ] ; then
 	mv /home/travis/gopath/bin/art /home/travis/gopath/bin/art.exe
 fi
+
 echo c
 ls
