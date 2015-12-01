57b16c8dc9800075d9838704378d5d3ed222a51a
shikloshi <aviads@jfrog.com>
2015-12-01 15:41:55 +0200

    build script

--- prepare_bintray_deployment.sh
+++ prepare_bintray_deployment.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-echo a
-ls
 
 cd /home/travis/gopath/bin
-echo "$(GOOS)"
-echo "$(GOARCH)"
-ls
 
-if [ "$(GOOS)" = "windows" ] ; then
-	mv /home/travis/gopath/bin/art /home/travis/gopath/bin/art.exe
+if [ "${GIMME_OS}" = "windows" ] ; then
+	mv art art.exe
 fi
-
-echo c
-ls
