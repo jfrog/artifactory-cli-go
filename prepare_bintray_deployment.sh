df3464d10969f9d85520e273bcfeca47960d0327
shikloshi <aviads@jfrog.com>
2015-12-02 11:50:20 +0200

    build descriptor

--- prepare_bintray_deployment.sh
+++ prepare_bintray_deployment.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-cd /home/travis/gopath/bin
+# cd /home/travis/gopath/bin
 
 echo a
 ls
 
-
-if [ "${GIMME_OS}" != "linux" ] || ["${GIMME_ARCH}" != "amd64"] ; then
-	cd "${GIMME_OS}_${GIMME_ARCH}"
-fi
+#
+# if [ "${GIMME_OS}" != "linux" ] || ["${GIMME_ARCH}" != "amd64"] ; then
+# 	cd "${GIMME_OS}_${GIMME_ARCH}"
+# fi
 
 
 echo b
 ls
 
 if [ "${GIMME_OS}" = "windows" ] ; then
-	mv art art.exe
+	mv /home/travis/gopath/bin/windows_amd64/art /home/travis/gopath/bin/windows_amd64/art.exe
 fi
 
 echo c
