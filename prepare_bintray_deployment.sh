0baba1f4d0e1f95a258e7f6435997384e408d537
shikloshi <aviads@jfrog.com>
2015-12-02 14:08:04 +0200

    build script

--- prepare_bintray_deployment.sh
+++ prepare_bintray_deployment.sh
@@ -2,21 +2,6 @@
 
 # cd /home/travis/gopath/bin
 
-echo a
-ls
-
-#
-# if [ "${GIMME_OS}" != "linux" ] || ["${GIMME_ARCH}" != "amd64"] ; then
-# 	cd "${GIMME_OS}_${GIMME_ARCH}"
-# fi
-
-
-echo b
-ls
-
-if [ "${GIMME_OS}" = "windows" ] ; then
+if [ "${OS}" = "windows" ] ; then
 	mv /home/travis/gopath/bin/windows_amd64/art /home/travis/gopath/bin/windows_amd64/art.exe
 fi
-
-echo c
-ls
