f72172f4f5ff65497abc5dd37bcbef019e3b93b8
eyalbe4 <eyalbenmoshe@jfrog.com>
2015-12-20 18:12:03 +0200

    Build script changes.

--- prepare_bintray_deployment.sh
+++ prepare_bintray_deployment.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 
-# cd /home/travis/gopath/bin
-
-if [ "${GIMME_OS}" = "windows" ] ; then
-	mv /home/travis/gopath/bin/windows_amd64/art /home/travis/gopath/bin/windows_amd64/art.exe
-fi
